a4738b23221bfdb41dc25da284b31d58bd2f2c9d
markoy1928 <alexpmarquis@gmail.com>
2025-04-02 10:27:47 -0400

    Added different sprites for each game

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,10 +1,64 @@
 import React from "react";
 import data from "./game_data.json";
 
+function getGame() {
+    switch (data.game) {
+        case "RB":
+            return "red-blue";
+
+        case "Y":
+            return "yellow";
+
+        case "GS":
+            return "gold";
+
+        case "C":
+            return "crystal";
+
+        case "RS":
+            return "ruby-sapphire";
+
+        case "E":
+            return "emerald";
+
+        case "FRLG":
+            return "firered-leafgreen";
+
+        case "DP":
+            return "diamond-pearl";
+
+        case "PT":
+            return "platinum";
+
+        case "HGSS":
+            return "heartgold-soulsilver";
+
+        case "BW":
+            return "black-white";
+
+        case "B2W2":
+            return "black-white";
+
+        case "XY":
+            return "x-y";
+
+        case "ORAS":
+            return "omega-ruby-alpha-sapphire";
+
+        case "SM":
+            return "sun-moon";
+
+        case "USUM":
+            return "ultra-sun-ultra-moon";
+        default:
+            return "platinum"
+    }
+}
+
 function Pokemon(partyPosition) {
     let mon = data.party[partyPosition];
 
-    if (mon.dex != 0) {
+    if (mon.dex !== 0) {
         let hpPercent = 100 * (mon.hp / mon.maxHP)
         let hpColor
 
@@ -18,10 +72,12 @@ function Pokemon(partyPosition) {
             hpColor = "#cc0000";
         }
 
+        let imgURL = "https://img.pokemondb.net/sprites/" + getGame() + "/normal/cyndaquil.png"
+
         return (
             <div class="Pokemon">
                 <p>{mon.nickname}</p>
-                <img src="https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/cyndaquil.png" alt="Mew"></img>
+                <img src={imgURL} alt="Mew"></img>
                 <div>
                     <div class="Health">
                         <div class="HP" style={{width: hpPercent + "%", backgroundColor: hpColor}}></div>
